5e1ab2d0ce0c067826cb2bfeec88af8da8edb8c6
lkhang <socbaycff@gmail.com>
2020-08-09 20:14:55 +0700

    Initial Commit

---
+++ HeroLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeroLearn
+//
+//  Created by L Khang on 8/9/20.
+//  Copyright © 2020 L Khang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
